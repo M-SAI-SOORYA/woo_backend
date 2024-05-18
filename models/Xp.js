14ced540305371061d7fbd0779776a802bb1c31c
Marri Sai Soorya <96968484+M-SAI-SOORYA@users.noreply.github.com>
2024-05-18 20:47:09 +0530

    Update Xp.js

--- models/Xp.js
+++ models/Xp.js
@@ -2,7 +2,9 @@ const mongoose=require('mongoose');
 const mongoURI = process.env.MONGO_URI || "mongodb+srv://saisooryamarri:Hs8iRlr1mfhvMAKB@system.vesozdf.mongodb.net/?retryWrites=true&w=majority&appName=System"
 mongoose.connect(mongoURI, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 30000, // Increase the server selection timeout
+    socketTimeoutMS: 45000 // Increase the socket timeout
 })
 .then(() => console.log('MongoDB connected'))
 .catch(err => console.error('MongoDB connection error:', err));const xpSchema=new mongoose.Schema({
